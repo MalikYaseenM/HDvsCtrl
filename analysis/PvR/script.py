27157fe6c9562d1279650d11cd9caad44cea9406
crespodi <crespodi@bu.edu>
2017-04-14 18:11:41 -0400

    created a script that parsed out sequence names and whether they were poly-A or ribo-depleted

--- analysis/PvR/script.py
+++ analysis/PvR/script.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import os
 
+created = 'samplepoly.tsv'
 fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
-files = pd.read_csv(fn, sep=',')
-print(files)
+#col_names = ['Dataset.dataset_id']
+col_names = ['Dataset.dataset_id','Dataset.protocol']
+#files = pd.read_csv(fn, sep=',', comment='#', names=col_names)
+files = pd.read_csv(fn, sep=',', comment='#', usecols=col_names)
+files.to_csv('protocol.tsv', sep='\t')
+#print(files)
+
+# with open(created, 'w')as o: 
+#     with open(files, 'r') as f:
+#         for rows in f:
+#             if 'poly-A' in rows:
+#                 listpoly.append(rows)
+#         print(listpoly[0])
+        
+
+
+#Poly_A = [ _ for _ in files.protocol if _.startswith('poly-A')]
+
+#Ribo_dep = [ _ for _ in dataset_ids if _.startswith('Ribo-depleted')]
+
