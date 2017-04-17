90928f4e5458ac9cfa56df38b56fdedade9c1176
Filisia Agus <fiagus@scc4.bu.edu>
2017-04-17 16:39:05 -0400

    modified r script to pull design info, modified python script to output 'design', added all outputs

--- analysis/diff_exp/common_DF.py
+++ analysis/diff_exp/common_DF.py
@@ -2,10 +2,18 @@ import pandas
 import os
 
 fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
-col_names = ['Dataset.dataset_id']
+col_names = ['Dataset.dataset_id','Subject.subject_type','Subject.death']
 
 sample_info = pandas.read_csv('../HD_mRNASeq_sample_info.csv', comment='#')
 
 files = pandas.read_csv(fn, sep=',', comment='#', usecols=col_names)
 
-files.reindex(columns=['Dataset.dataset_id']).to_csv('sample_info.csv', index=False,header=False)
+files.reindex(columns=['Dataset.dataset_id','Subject.subject_type','Subject.death']).to_csv('sample_info_py.csv',sep=',', index=False,header=False)
+
+#files.reindex(columns=['Dataset.dataset_id']).to_csv('sample_info_pytest.csv',sep=',', index=False,header=False)
+
+#files.reindex(columns=['Subject.subject_type','Subject.death']).to_csv('sample_design_py.csv',sep='~', index=False,header=False)
+
+#fn = os.path.abspath('sample_info.csv')
+
+
--- analysis/diff_exp/pull_sample.R
+++ analysis/diff_exp/pull_sample.R
@@ -23,4 +23,25 @@ sample_name <- sub("BA9$","BA9_mRNASeq",sample_name)
 sample_name <- as.data.frame(sample_name)
 
 # Write to csv file
-write.table(sample_name,"sample_info.csv",col.names=FALSE,quote=FALSE,row.names=FALSE,sep=",")
+write.table(sample_name,"sample_info_norm_order.csv",col.names=FALSE,quote=FALSE,row.names=FALSE,sep=",")
+
+# Initialize ordered samples as ordered
+ordered <- read.csv("sample_info_norm_order.csv", header=FALSE)
+ordered <- as.data.frame(ordered)
+
+# Initialize original samples from python script output
+original <- read.csv("sample_info_py.csv", header=FALSE)
+original <- as.data.frame(original)
+row.names(original) <- original$V1
+row.names(ordered) <- ordered$V1
+
+# Reorder original (append into ordered) and rename as original2
+original2 <- original[row.names(ordered),]
+
+# Pull subject type and age of death from original2
+ordered_design <- original2[,(2:3)]
+# Merge the 2 columns and add ~
+ordered_design2 <- as.data.frame(paste(original2$V2, original2$V3, sep=" ~ "))
+
+# Write to csv file
+write.table(ordered_design2,"sample_info_design.csv",col.names=FALSE,quote=FALSE,row.names=FALSE,sep=",")
