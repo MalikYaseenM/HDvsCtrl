809bc69f5289c79927c148a1b0e18bfbdf4f4376
Filisia Agus <fiagus@scc4.bu.edu>
2017-04-17 17:19:07 -0400

    modified py to create new normalized counts without header, added output

--- analysis/diff_exp/common_DF.py
+++ analysis/diff_exp/common_DF.py
@@ -8,12 +8,17 @@ sample_info = pandas.read_csv('../HD_mRNASeq_sample_info.csv', comment='#')
 
 files = pandas.read_csv(fn, sep=',', comment='#', usecols=col_names)
 
+# To get sample name, subject type and age of death all in one file
 files.reindex(columns=['Dataset.dataset_id','Subject.subject_type','Subject.death']).to_csv('sample_info_py.csv',sep=',', index=False,header=False)
 
 #files.reindex(columns=['Dataset.dataset_id']).to_csv('sample_info_pytest.csv',sep=',', index=False,header=False)
 
 #files.reindex(columns=['Subject.subject_type','Subject.death']).to_csv('sample_design_py.csv',sep='~', index=False,header=False)
 
-#fn = os.path.abspath('sample_info.csv')
+fl = os.path.abspath('../../samples/all_salmon_quant.tsv')
 
-
+with open(fl,'r') as f:
+    with open("normalized_salmon_wo_header.tsv",'w') as f1:
+        next(f) # skip header line
+        for line in f:
+            f1.write(line)
