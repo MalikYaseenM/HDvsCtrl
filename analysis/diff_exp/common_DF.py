044644c3cdb4570460ca0825c1094e4f6e62d7b0
Filisia Agus <fiagus@scc4.bu.edu>
2017-04-18 13:20:21 -0400

    modified salmonsnake so its output will have _mRNASeq on name

--- analysis/diff_exp/common_DF.py
+++ analysis/diff_exp/common_DF.py
@@ -11,10 +11,14 @@ files = pandas.read_csv(fn, sep=',', comment='#', usecols=col_names)
 # To get sample name, subject type and age of death all in one file
 files.reindex(columns=['Dataset.dataset_id','Subject.subject_type','Subject.death']).to_csv('sample_info_py.csv',sep=',', index=False,header=False)
 
+# To get sample name only
 #files.reindex(columns=['Dataset.dataset_id']).to_csv('sample_info_pytest.csv',sep=',', index=False,header=False)
 
+# To get subject type and age of death only
 #files.reindex(columns=['Subject.subject_type','Subject.death']).to_csv('sample_design_py.csv',sep='~', index=False,header=False)
 
+
+# Makes new normalized salmon count file with no header
 fl = os.path.abspath('../../samples/all_salmon_quant.tsv')
 
 with open(fl,'r') as f:
@@ -22,3 +26,5 @@ with open(fl,'r') as f:
         next(f) # skip header line
         for line in f:
             f1.write(line)
+
+
--- analysis/diff_exp/pull_sample.R
+++ analysis/diff_exp/pull_sample.R
@@ -8,26 +8,24 @@ counts <- read.table(opts$count_matrix, header=TRUE, row.names = 'gene_id')
 counts <- as.matrix(counts)
 
 # Taking out the sample names from the column names and saving it as a data frame
-sample_names <- (colnames(counts))
 colData <- as.data.frame(colnames(counts))
 
-# Make new colData as sample names
-# Some colData had __salmon__counts ending (on sample names with REPEAT)
-sample_name <- sub("__salmon__counts","",colData[,1])
+## Make new colData as sample names
+## Some colData had __salmon__counts ending (on sample names with REPEAT)
+#sample_name <- sub("__salmon__counts","",colData[,1])
 
-# Sample names didn't have mRNASeq, added mRNASeq to all
-sample_name <- sub("CAP$","CAP_mRNASeq",sample_name)
-sample_name <- sub("BA9$","BA9_mRNASeq",sample_name)
+## Sample names didn't have mRNASeq, added mRNASeq to all
+#sample_name <- sub("CAP$","CAP_mRNASeq",sample_name)
+#sample_name <- sub("BA9$","BA9_mRNASeq",sample_name)
 
-# Set sample name as a data frame
-sample_name <- as.data.frame(sample_name)
+## Set sample name as a data frame
+#sample_name <- as.data.frame(sample_name)
 
 # Write to csv file
-write.table(sample_name,"sample_info_norm_order.csv",col.names=FALSE,quote=FALSE,row.names=FALSE,sep=",")
+#write.table(sample_name,"sample_info_norm_order.csv",col.names=FALSE,quote=FALSE,row.names=FALSE,sep=",")
 
-# Initialize ordered samples as ordered
-ordered <- read.csv("sample_info_norm_order.csv", header=FALSE)
-ordered <- as.data.frame(ordered)
+# Initialize ordered samples from colData
+ordered <- colData
 
 # Initialize original samples from python script output
 original <- read.csv("sample_info_py.csv", header=FALSE)
