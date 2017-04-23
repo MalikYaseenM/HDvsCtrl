417370806a62393da4a8ea75fa3a23d62b5f8750
crespodi <crespodi@bu.edu>
2017-04-23 15:25:31 -0400

    created csv containing names of samples that were polyA or ribodep, currently modifying the scripts to run throught snakemake

--- analysis/PvR/script.py
+++ analysis/PvR/script.py
@@ -5,8 +5,5 @@ fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
 
 col_names = ['Dataset.dataset_id','Dataset.protocol']
 
-files = pd.read_csv(fn, sep=',', comment='#', usecols=col_names)
-
-#files.to_csv('protocol.tsv', sep='\t')
-
-files.reindex(columns=['Dataset.protocol','Dataset.dataset_id']).to_csv('sample_info.csv', sep=',', index=False,header=False)
+readin = pd.read_csv(fn, sep=',', comment='#', usecols=col_names)
+readin.reindex(columns=['Dataset.protocol','Dataset.dataset_id']).to_csv('sample_info.csv', sep=',', index=False,header=False)
