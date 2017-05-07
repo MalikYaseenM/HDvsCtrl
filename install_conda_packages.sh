54e849afb977b00deec654dfcf0068a98f56cd36
Filisia Agus <fiagus@scc4.bu.edu>
2017-05-07 00:38:20 -0400

    install samtools

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,2 +1,2 @@
-conda install python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon de_toolkit
+conda install python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon de_toolkit samtools
 conda list --export > conda_packages.txt
