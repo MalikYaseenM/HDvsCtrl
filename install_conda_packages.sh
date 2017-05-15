b8242c5ff13a72a9107cec7f5429bd8b1b9b2edd
crespodi <crespodi@bu.edu>
2017-05-15 13:31:48 -0400

    added verse to the install conda packages, created a rule for verse in the Snakefile

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,2 +1,2 @@
-conda install python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon de_toolkit samtools
+conda install python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon de_toolkit samtools verse
 conda list --export > conda_packages.txt
