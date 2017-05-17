52a2292c9881617c656a0d777962a24e217b6c70
Filisia Agus <fiagus@scc4.bu.edu>
2017-05-17 18:51:14 -0400

    modified snakefile for cap

--- analysis/CAP_Analysis/design.py
+++ analysis/CAP_Analysis/design.py
@@ -3,7 +3,7 @@ import os
 
 fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
 
-fl = os.path.abspath('../diff_exp/all_salmon_norm.csv')
+fl = os.path.abspath('../../samples/all_salmon_quant_rrna.tsv')
 
 # Read sample info
 samples = pd.read_csv(fn, sep=",", comment='#')
@@ -11,13 +11,25 @@ samples = pd.read_csv(fn, sep=",", comment='#')
 sample_info = samples.iloc[:,[14,1,4]]
 # Pulls only CAP samples
 sample_info = sample_info[sample_info['Dataset.dataset_id'].str.contains("CAP")]
+# To get HD or control
+#control_ids = [ _ for _ in sample_info['Dataset.dataset_id'] if _.startswith('C')]
+#HD_ids = [ _ for _ in sample_info['Dataset.dataset_id'] if _.startswith('H')]
 
 # Pulls only CAP samples from counts file
-df = pd.read_csv(fl, sep=',', comment='#')
+df = pd.read_csv(fl, sep='\t', comment='#')
 df.drop([col for col in df.columns if 'BA9' in col],axis=1,inplace=True)
-# Creates new file with only CAP samples
-df.to_csv("CAP_salmon_norm.csv", index=False)
 
+# Filtering, dropping any rows with a 0
+df = df[(df != 0).all(1)]
+
+# Filtering
+#df['avg_control'] = df[control_ids].mean(axis=1)
+#df['avg_HD'] = df[HD_ids].mean(axis=1)
+#df = df[(df.avg_control > 5) & (df.avg_HD > 5)]
+#df = df.drop('avg_control', axis=1)
+#df = df.drop('avg_HD', axis=1)
+# Creates new file with only CAP samples
+df.to_csv("CAP_salmon_filter.csv", index=False)
 
 # For sample_info design
 sample_i = pd.DataFrame(df.columns)
