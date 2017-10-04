9e60747b023050de50039fb75b0909e25192c14b
crespodi <crespodi@bu.edu>
2017-10-03 23:56:09 -0400

    updated install conda with the new packages for detk as well as took steps to decrease shared library errors

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,2 +1,2 @@
-conda install python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon de_toolkit samtools verse
+conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon samtools verse
 conda list --export > conda_packages.txt
