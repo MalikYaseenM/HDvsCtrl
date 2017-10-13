e1d6a4e33723b1ad384f9ef168bd80f4ce06a6b9
Filisia Agus <fiagus@scc4.bu.edu>
2017-10-13 14:24:39 -0400

    total BA9 analysis

--- analysis/BA9_analysis/BA9_design.py
+++ analysis/BA9_analysis/BA9_design.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 
+# HD mRNASeq sample info
 fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
-
+# Counts matrix
 fl = os.path.abspath('../../samples/all_salmon_quant_rrna.tsv')
 
 # Read sample info
@@ -10,10 +11,13 @@ samples = pd.read_csv(fn, sep=",", comment='#')
 # column15: datasetid, 2: subject type, 5: death age
 sample_info = samples.iloc[:,[14,1,4]]
 # Pulls only BA9 samples
-sample_info = sample_info[sample_info['Dataset.dataset_id'].str.contains("BA9")]
+BA9 = sample_info[sample_info['Dataset.dataset_id'].str.contains("BA9")].copy()
+# Change column names from . to _
+BA9.columns = ["Data_id","Subject_type","Subject_death"]
+
 # To get HD or control
-control_ids = [ _ for _ in sample_info['Dataset.dataset_id'] if _.startswith('C')]
-HD_ids = [ _ for _ in sample_info['Dataset.dataset_id'] if _.startswith('H')]
+control_ids = [ _ for _ in BA9['Data_id'] if _.startswith('C')]
+HD_ids = [ _ for _ in BA9['Data_id'] if _.startswith('H')]
 
 # Pulls only BA9 samples from counts file
 df = pd.read_csv(fl, sep='\t', comment='#')
@@ -30,6 +34,6 @@ df.to_csv("BA9_salmon_filter.csv", index=False)
 # For sample_info design
 sample_i = pd.DataFrame(df.columns)
 sample_i = sample_i.drop(0)
-sample_i = sample_i.rename(columns = {0:'Dataset.dataset_id'})
-df_new = pd.merge(sample_i,sample_info, on='Dataset.dataset_id')
+sample_i = sample_i.rename(columns = {0:'Data_id'})
+df_new = pd.merge(sample_i,BA9, on='Data_id')
 df_new.to_csv("BA9_info_design.csv", index=False)
