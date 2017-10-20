a9512f2cbb64391cb6caad2a4f1e059c39e45319
Filisia Agus <fiagus@scc4.bu.edu>
2017-10-20 11:20:53 -0400

    modified asymp analysis files

--- analysis/asymp_analysis/asymp_design.py
+++ analysis/asymp_analysis/asymp_design.py
@@ -6,6 +6,10 @@ fn = os.path.abspath('../HD_mRNASeq_sample_info.csv')
 # Counts matrix
 fl = os.path.abspath('../../samples/all_salmon_quant_rrna.tsv')
 
+# Read data
+samples = pd.read_csv(fn, sep=",", comment='#')
+df = pd.read_csv(fl, sep='\t', comment='#')
+
 # Append the 2 batches for asymp (HDpos) datasets, 5 samples total
 batch1 = samples[samples['Dataset.batch']=="mRNA_asymHD"]
 batch2 = samples[samples['Dataset.batch']=="mRNA_HD3"]
@@ -24,36 +28,29 @@ sample_info.loc[:,'Subject_type'] = sample_info["Data_id"].map(lambda x: "BA9" i
 
 ######################### Filtering #######################
 col = list(df)
-cols_BA9 = col[:1] + batch_BA9
-cols_CAP = col[:1] + batch_CAP
+cols = col[:1] + batch_id
 
-# Makes 2 new dataframes to drop 0s
-df_BA9 = salmon.drop([col for col in salmon.columns if col not in cols_BA9],axis=1, inplace=False)
-df_CAP = salmon.drop([col for col in salmon.columns if col not in cols_CAP],axis=1, inplace=False)
-df_BA9 = df_BA9[(df_BA9 != 0).all(1)]
-df_CAP = df_CAP[(df_CAP != 0).all(1)]
+# Drop rows if column names not in batch_id
+df.drop([col for col in df.columns if col not in cols],axis=1,inplace=True)
 
-# Merge the 2 dataframes by index
-df_batch = pd.concat([df_BA9, df_CAP], axis=1)
-# Drop duplicate gene_id column
-df_batch = df_batch.loc[:,~df_batch.columns.duplicated()]
+# RAW FILTERING STEP 1: Drop rows with any 0s
+df = df[(df != 0).all(1)]
 
-# Drop rows with na values
-df_batch = df_batch.dropna(axis=0, how='any')
 # Find average
-df_batch['avg_BA9'] = df_batch[batch_BA9].mean(axis=1)
-df_batch['avg_CAP'] = df_batch[batch_CAP].mean(axis=1)
+df['avg_BA9'] = df[batch_BA9].mean(axis=1)
+df['avg_CAP'] = df[batch_CAP].mean(axis=1)
 
-# Drops rows if avg_BA < 5 or avg_CAP < 5
-df_batch = df_batch[(df_batch.avg_BA9 > 5) | (df_batch.avg_CAP > 5)]
+# RAW FILTERING STEP 2: Drops rows if avg_BA < 5 or avg_CAP < 5
+df = df[(df.avg_BA9 > 5) | (df.avg_CAP > 5)]
 # Drops columns
-df_batch = df_batch.drop('avg_BA9', axis=1)
-df_batch = df_batch.drop('avg_CAP', axis=1)
+df = df.drop('avg_BA9', axis=1)
+df = df.drop('avg_CAP', axis=1)
 
 # Creates new file with only CAP samples
-df_batch.to_csv("asymp_salmon_filter.csv", index=False)
+df.to_csv("asymp_salmon_filter.csv", index=False)
 
-sample_i = pd.DataFrame(df_batch.columns)
+######################### Merge for design info  #######################
+sample_i = pd.DataFrame(df.columns)
 sample_i = sample_i.drop(0)
 sample_i = sample_i.rename(columns = {0:'Data_id'})
 idesign = pd.merge(sample_i,sample_info, on='Data_id')
