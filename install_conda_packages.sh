8b26c3613ab5c9401796b57181248b8a587fb330
crespodi <crespodi@bu.edu>
2017-10-29 22:55:25 -0400

    updated the install conda script, removed detk package

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,2 +1,2 @@
-conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc trimmomatic multiqc snakemake pandas salmon samtools verse
+conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc trimmomatic=0.3.5 multiqc snakemake pandas salmon samtools verse
 conda list --export > conda_packages.txt
