692c9dcb169d1af71175dfb5af7eecf16e917572
crespodi <crespodi@bu.edu>
2017-11-01 14:58:21 -0400

    updated the workflow.py with some comments

--- analysis/PvR/workflow.py
+++ analysis/PvR/workflow.py
@@ -9,13 +9,13 @@ for lines in fullread:
     if 'poly-A' in lines:
         newline= lines.strip('\n')
         split = newline.split(',')
-        Poly.append(split[1])
+        Poly.append(split[1]) # append the name of the poly-A selected sample
     else:
         newline= lines.strip('\n')
         split = newline.split(',')
-        Ribo.append(split[1])
+        Ribo.append(split[1]) # append the name of the ribo-depleted sample
 
 polyseq=pd.DataFrame(Poly, columns=['Poly_A'])
-polyseq.to_csv('polyseq.csv', sep=',', index=False)
-ribodepseq=pd.DataFrame(Ribo, columns=['Ribo_D'])
-ribodepseq.to_csv('ribodepseq.csv', sep=',', index=False)
+polyseq.to_csv('polyseq.csv', sep=',', index=False) # Create a csv which just contains the poly-A samples
+ribodepseq=pd.DataFrame(Ribo, columns=['Ribo_D']) 
+ribodepseq.to_csv('ribodepseq.csv', sep=',', index=False) # creeate a csv which just contains the ribo-depleted samples
