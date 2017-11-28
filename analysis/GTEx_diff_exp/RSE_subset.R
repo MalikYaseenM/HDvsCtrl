a92d9e1ef0837d2b5fc873ce7b5706e3bfd53d23
crespodi <crespodi@bu.edu>
2017-11-28 16:19:51 -0500

    RSE_subset only looks at rows that are concordant with filtered gtex data

---
+++ analysis/GTEx_diff_exp/RSE_subset.R
@@ -0,0 +1,25 @@
+# DE Analysis of RNA-seq data
+
+##load libraries
+## design=~ SEX + AGE 
+.libPaths( c( .libPaths(), '/projectnb/bubhub/conda_root/user_envs/crespodi/presymptomatic_hd_mrnaseq/lib/R/library') )
+library(DESeq2)
+## loading in the files
+RSE_file <- '../../samples/GTEx/results/subset_RSE.csv'
+meta_data <- '../../samples/GTEx/results/new_sample_info.csv'
+##meta data
+colData <- read.csv(meta_data, header=TRUE,stringsAsFactors=FALSE)
+colData$SEX <- as.factor(colData$SEX)
+colData$brain_region <- as.factor(colData$brain_region)
+
+##RSE data
+counts <- read.csv(RSE_file, header=TRUE, row.names = 'gene_id')
+counts <- as.matrix(counts)
+storage.mode(counts) <- 'integer'
+dds <- DESeqDataSetFromMatrix(countData = counts,
+                              colData = colData,
+                              design = ~ brain_region)
+dds$brain_region<- relevel(dds$brain_region, ref='CAU')
+dds <- DESeq(dds, minReplicatesForReplace = Inf)
+res <- results(dds, cooksCutoff = Inf)
+write.csv(res, file = '../../samples/GTEx/results/RSE_subset_deseq2_results.csv', quote = FALSE)
