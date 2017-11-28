ccc65d83c77f56a6d289ce7e70fada9d53d580d8
crespodi <crespodi@bu.edu>
2017-11-28 16:16:15 -0500

    updated deseq2 R scripts for recount and GTEX data

--- analysis/GTEx_diff_exp/RSE_deseq2_filter.R
+++ analysis/GTEx_diff_exp/RSE_deseq2_filter.R
@@ -18,8 +18,8 @@ counts <- as.matrix(counts)
 storage.mode(counts) <- 'integer'
 dds <- DESeqDataSetFromMatrix(countData = counts,
                               colData = colData,
-                              design = ~ AGE + SEX + brain_region)
+                              design = ~ brain_region)
 dds$brain_region<- relevel(dds$brain_region, ref='CAU')
-dds <- DESeq(dds)
-res <- results(dds)
+dds <- DESeq(dds, minReplicatesForReplace = Inf)
+res <- results(dds, cooksCutoff = Inf)
 write.csv(res, file = '../../samples/GTEx/results/RSE_filter_deseq2_results.csv', quote = FALSE)
