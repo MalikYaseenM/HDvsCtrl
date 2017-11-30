5705b187d3b0972938eacae1de14acf1bab368cf
crespodi <crespodi@bu.edu>
2017-11-29 20:34:24 -0500

    sex_stratification.py updated with comments to code, new file paths for outputs

--- analysis/GTEx_diff_exp/sex_stratification.py
+++ analysis/GTEx_diff_exp/sex_stratification.py
@@ -1,12 +1,17 @@
-import pandas as pd
+import pandas as pd, os
+#adds gene id to the usecols list
 foo = ['gene_id']
-male = foo + pd.read_csv('male_sample_design_info.csv',
-                         usecols=['identifier'])['identifier'].tolist()
-female = foo + pd.read_csv('female_sample_design_info.csv',
-                           usecols=['identifier'])['identifier'].tolist()
 
-male_df = pd.read_csv('GTEX_salmon_norm.csv', sep=',', usecols=male)
+# list with gene_id and female or male subjects
+male = foo + pd.read_csv(os.path.abspath("../../samples/GTEx/results/male_sample_design_info.csv"),usecols=['identifier'])['identifier'].tolist()
+female = foo + pd.read_csv(os.path.abspath("../../samples/GTEx/results/female_sample_design_info.csv"),usecols=['identifier'])['identifier'].tolist()
+
+# read in the normalized counts use only the males
+male_df = pd.read_csv(os.path.abspath("../../samples/GTEx/results/GTEX_salmon_norm.csv"), sep=',', usecols=male)
+
+# read in the normalized counts use only the females
 female_df = pd.read_csv('GTEX_salmon_norm.csv', sep=',', usecols=female)
 
-male_df.to_csv("GTEX_male_salmon_norm.csv", sep=',', index=False)
-female_df.to_csv("GTEX_female_salmon_norm.csv", sep=',', index=False)
+# write out seperate files for males and females
+male_df.to_csv(os.path.abspath("../../samples/GTEx/results/GTEX_male_salmon_norm.csv"), sep=',', index=False)
+female_df.to_csv(os.path.abspath("../../samples/GTEx/results/GTEX_female_salmon_norm.csv"), sep=',', index=False)
