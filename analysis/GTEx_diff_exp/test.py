c252e59668cfb4e433f7552e994c156a4cbb350d
crespodi <crespodi@bu.edu>
2018-02-08 12:34:46 -0500

    added new lab meeting information '

--- analysis/GTEx_diff_exp/test.py
+++ analysis/GTEx_diff_exp/test.py
@@ -1,18 +1,19 @@
-import pandas as pd
+#import pandas as pd
 
-phenotype_data = 'phs000424.v7.pht002742.v7.p2.c1.GTEx_Subject_Phenotypes.GRU.txt'
-meta_file = pd.read_csv('sample_info_design.csv', sep=',')
+
+# phenotype_data = 'phs000424.v7.pht002742.v7.p2.c1.GTEx_Subject_Phenotypes.GRU.txt'
+# meta_file = pd.read_csv('sample_info_design.csv', sep=',')
 
 
 
 # #GTEX-1117F
 # #[0:9]
 # #GTEX-11GSP_BA9_mRNASeq
-df =  pd.read_csv(phenotype_data, sep='\t', comment='#')
-df = df.head()
-meta_file = meta_file.head()
-meta_file.to_csv('shortsample.csv', sep=',')
-df.to_csv('shortpheno.tsv', sep='\t')
+# df =  pd.read_csv(phenotype_data, sep='\t', comment='#')
+# df = df.head()
+# meta_file = meta_file.head()
+# meta_file.to_csv('shortsample.csv', sep=',')
+# df.to_csv('shortpheno.tsv', sep='\t')
 
 # df = df[['SEX','AGE','SUBJID']]
 # SUBJID = df['SUBJID'].values.tolist()
