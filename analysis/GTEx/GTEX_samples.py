a2813350fdbc8ba58ef04975f68f5bd0d2cdcca2
crespodi <crespodi@bu.edu>
2018-02-25 18:36:00 -0500

    removed gtex_samples.py and added it directly to the Snakefile

--- analysis/GTEx/GTEX_samples.py
+++
@@ -1,7 +0,0 @@
-import csv
-import pandas
-#usecols=col_names)
-col_names = ['identifier', 'sample_name', 'brain_region', 'abspath']
-df = pandas.read_csv('GTEX_info.tsv', sep='\t', usecols=col_names)
-
-samples = df['identifier'].tolist()
