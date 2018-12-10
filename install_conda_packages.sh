c92435a5e0d470e7394f095b568062a361ceac80
Adam Labadorf <labadorf@bu.edu>
2018-12-10 10:33:31 -0500

    added matplotlib-venn for 3 vs 5 analysis

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,2 +1,4 @@
-conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc trimmomatic=0.3.5 multiqc snakemake pandas salmon samtools verse
+conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc \
+    trimmomatic multiqc snakemake pandas salmon samtools verse deeptools
 conda list --export > conda_packages.txt
+pip install networkx matplotlib-venn
