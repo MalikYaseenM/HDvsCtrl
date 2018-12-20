88166a8e14ed968b1825d8d9364736423c4b66dc
Adam Labadorf <alabadorf@gmail.com>
2018-12-20 15:23:22 -0500

    required package updates

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,4 +1,5 @@
 conda install -c conda-forge -c bioconda -f -c bubhub r python=3.5 star fastqc \
-    trimmomatic multiqc snakemake pandas salmon samtools verse deeptools
+    trimmomatic multiqc snakemake pandas salmon samtools verse deeptools \
+    graphviz
+pip install -r requirements.txt
 conda list --export > conda_packages.txt
-pip install networkx matplotlib-venn
